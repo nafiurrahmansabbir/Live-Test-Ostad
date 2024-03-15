17bc588a6d878ce1753cea6798892994055dedcc
sabbir <nafiurrahmansabbir@gmail.com>
2024-03-15 23:25:57 +0600

    yeee succesfully done

---
+++ Live_Test_01/live-test-01.dart
@@ -0,0 +1,32 @@
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': 'Purple', 'price': 3.0},
+  ];
+
+  print('Original Fruit Details before Discount:');
+  displayFruitDetails(fruits);
+
+  applyPriceDiscount(fruits, 10);
+
+  // Displaying fruit details after applying discount
+  print('Fruit Details After Applying 10% Discount:');
+  displayFruitDetails(fruits);
+}
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print(
+        'Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price'].toStringAsFixed(2)}');
+  }
+}
+
+void applyPriceDiscount(
+    List<Map<String, dynamic>> fruits, double discountPercentage) {
+  for (var fruit in fruits) {
+    double price = fruit['price'];
+    double discountedPrice = price - (price * discountPercentage / 100);
+    fruit['price'] = discountedPrice;
+  }
+}
